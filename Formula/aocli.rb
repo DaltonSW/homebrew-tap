4a1db1dc358cdc8c192e1a726a3ccc989dd84593
Dalton Williams <daltonstwilliams@gmail.com>
2024-08-23 16:22:16 -0500

    Added mac version

--- Formula/aocli.rb
+++ Formula/aocli.rb
@@ -1,12 +1,18 @@
 class aocli < Formula
   desc "A fast and pretty CLI program for interfacing with Advent of Code puzzles."
   homepage "https://github.com/DaltonSW/aocgo/tree/main/cmd/aocli"
-  url "https://github.com/DaltonSW/aocgo/releases/download/aocli-v0.9.8/aocli-linux-amd64"
-  sha256 "C9A58CA773436320868797B6583D9085BC3FF2EDEF5709D2E8C01EDC4CE0B6F0"
+  if OS.linux?
+    url "https://github.com/DaltonSW/aocgo/releases/download/aocli-v0.9.8/aocli-linux-amd64"
+    sha256 "C9A58CA773436320868797B6583D9085BC3FF2EDEF5709D2E8C01EDC4CE0B6F0"
+  elsif OS.mac?
+    url "https://github.com/DaltonSW/aocgo/releases/download/aocli-v0.9.8/aocli-darwin-amd64"
+    sha256 "F0924962BF82970F6877D72B9CB3E925C30D77019FF115BD946EC6D610A2BB4E"
+  end
+
   license "GPL-3.0-or-later"
 
   def install
-    bin.install "aocli-linux-amd64" => "aocli"
+    bin.install "aocli"
   end
 
   test do
