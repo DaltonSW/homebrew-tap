bb1ea20e6a73949af67ac89e3050ba250c7af847
Dalton Williams <daltonstwilliams@gmail.com>
2024-12-03 13:28:25 -0600

    Update aocli to v0.11.0

--- Formula/aocli.rb
+++ Formula/aocli.rb
@@ -2,11 +2,11 @@ class Aocli < Formula
   desc "A fast and pretty CLI program for interfacing with Advent of Code puzzles."
   homepage "https://github.com/DaltonSW/aocgo/tree/main/cmd/aocli"
   if OS.linux?
-    url "https://github.com/DaltonSW/aocgo/releases/download/aocli-v0.9.9/aocli-linux-amd64"
-    sha256 "0B08C377208681285A357660DF53FD5F233F54EB72BCC0BEB283082F874D7753"
+    url "https://github.com/DaltonSW/aocgo/releases/download/aocli-v0.11.0/aocli-linux-amd64"
+    sha256 "56eb5d6f9c74b2091f4cd1c66aac795ee0656e77a5a182ba3dd643669d50117d"
   elsif OS.mac?
-    url "https://github.com/DaltonSW/aocgo/releases/download/aocli-v0.9.9/aocli-darwin-amd64"
-    sha256 "83B5268C80EB2ED2A2F8AE0DD3BFD005B6A7CAC472DCE18CCFB02E3D91FA8BEF"
+    url "https://github.com/DaltonSW/aocgo/releases/download/aocli-v0.11.0/aocli-darwin-amd64"
+    sha256 "56eb5d6f9c74b2091f4cd1c66aac795ee0656e77a5a182ba3dd643669d50117d"
   end
 
   license "GPL-3.0-or-later"
